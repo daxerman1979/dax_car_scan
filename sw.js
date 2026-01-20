4eeeddb3d559e512cd86c7e7bacf8aa44b2dea45
daxerman1979 <daxerman79@gmail.com>
2026-01-20 12:41:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,11 @@
-const CACHE_NAME = 'dcs-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&family=Roboto:wght@700;900&display=swap',
-  'https://i.ibb.co/Lhb8Y6H/dcs-icon.png'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Skladistenje fajlova...');
-      return cache.addAll(assets);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('dcs-v1').then((cache) => cache.addAll(['/', '/index.html', '/manifest.json']))
   );
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
