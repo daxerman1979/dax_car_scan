98ce80316fd0dbc7ea84946eb42177fa259944f8
daxerman1979 <daxerman79@gmail.com>
2026-01-20 13:51:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,11 @@
-const CACHE_NAME = 'dcs-pro-v60'; // Promeni ovo svaki put kad menjaš kod
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&family=Roboto:wght@700;900&display=swap'
-];
-
-// Instalacija - čuvanje fajlova
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting(); // Forsira aktivaciju odmah
-});
-
-// Aktivacija - brisanje starog keša
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('dcs-v1').then((cache) => cache.addAll(['/index.html', '/manifest.json']))
   );
 });
 
-// Fetch - služenje fajlova iz keša kad nema neta
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
