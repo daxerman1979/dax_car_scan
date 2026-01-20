8e753780788f6762d71b36f3ee2a0068602323d1
daxerman1979 <daxerman79@gmail.com>
2026-01-20 12:17:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 const CACHE_NAME = 'dcs-v1';
 const assets = [
-  './',
-  './index.html',
-  './manifest.json',
+  'index.html',
+  'manifest.json',
   'https://fonts.googleapis.com/css2?family=Orbitron:wght@700;900&family=Roboto:wght@700;900&display=swap',
   'https://i.ibb.co/Lhb8Y6H/dcs-icon.png'
 ];
@@ -10,19 +9,7 @@ const assets = [
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Kesiranje fajlova...');
       return cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
     })
   );
 });
