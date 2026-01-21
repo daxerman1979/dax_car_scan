0fe4b600af9e3b56df1b14c6ad14fd3d44491cf0
daxerman1979 <daxerman79@gmail.com>
2026-01-21 11:46:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,10 @@
-const CACHE_NAME = 'dcs-pro-v61';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'dcs-v60-final';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => {
-      return res || fetch(evt.request);
-    })
-  );
+  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request)));
 });
