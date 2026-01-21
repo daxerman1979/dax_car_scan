facbccd42473c951e201dcb10e36ad3463efd706
daxerman1979 <daxerman79@gmail.com>
2026-01-21 11:40:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dcs-v60-pwa';
+const CACHE_NAME = 'dcs-pro-v61';
 const assets = [
   '/',
   '/index.html',
@@ -8,7 +8,19 @@ const assets = [
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
