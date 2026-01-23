b872b1852d397b681383f25f9f2e742e8ec6b56f
daxerman1979 <daxerman79@gmail.com>
2026-01-23 23:58:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = 'dcs-v70-github';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'dcs-v100-final-full';
+const assets = ['./', './index.html', './manifest.json'];
 
-// Instalacija i keširanje fajlova
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Skladištenje fajlova u keš...');
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting(); // Forsira aktivaciju nove verzije odmah
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
 });
 
-// Čišćenje starih keševa (važno da bi se uklonio stari Vercel keš)
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
+  evt.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+  }));
 });
 
-// Fetch strategija (prvo gleda keš, ako nema, ide na mrežu)
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request)));
 });
