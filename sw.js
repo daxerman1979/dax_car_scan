ca7f37e076e7a0aeacd2ceee9a6cf9139d084c91
daxerman1979 <daxerman79@gmail.com>
2026-01-24 15:23:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,18 @@
-const CACHE_NAME = 'dcs-v60-final-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'dcs-v210-final';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
+  evt.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+  }));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request)));
 });
